51b61ddfb7cbbe399a1ff0b9f8a6ec78fc5fd368
DmitryTeterkin <teterkin@tut.by>
2017-01-20 14:12:02 +0300

    Упражнение 2, добавил коментарии

--- sandbox/src/main/java/workbook/secondExercise/Point.java
+++ sandbox/src/main/java/workbook/secondExercise/Point.java
@@ -1,7 +1,6 @@
 package workbook.secondExercise;
 
 /**
- * Created by dteterkin on 19.01.2017.
  *
  * Создаем класс Point для задания двух точек и функцию, которая будет его вычислять.
  */
@@ -10,20 +9,20 @@ public class Point {
   public double x; // координата точки по оси Х;
   public double y; // координата точки по оси Y;
 
-
+// функция
   public static double distanceFuncion(Point p1, Point p2) {
 
     return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
   }
 
-
+//метод
   public double distanceMethod(Point p1, Point p2) {
 
     return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
   }
 
 
- //  Конструктор для создания точек
+ //  Конструктор
  Point(double x, double y) {
  this.x = x;
  this.y = y;
