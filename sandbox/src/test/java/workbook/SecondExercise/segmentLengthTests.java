660c4b45cd0df81942178fe47dd6cbacf25675c3
DmitryTeterkin <teterkin@tut.by>
2017-01-25 20:28:59 +0300

    Тесты проверки длины расстояния между точками. Задание 3

--- sandbox/src/test/java/workbook/SecondExercise/segmentLengthTests.java
+++ sandbox/src/test/java/workbook/SecondExercise/segmentLengthTests.java
@@ -1,7 +1,24 @@
 package workbook.secondExercise;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 /**
  * Created by dteterkin on 25.01.2017.
  */
 public class segmentLengthTests {
+
+  @Test
+
+  public void testdistanceMethod() {
+
+    Point p1 = new Point(12, 13);
+    Point p2 = new Point(-12, -13);
+
+    Point p3 = new Point(-12, 13);
+    Point p4 = new Point(-2, -13);
+    Assert.assertEquals(Point.distanceFuncion(p3, p4), 27.85677655436824);
+
+
+  }
 }
