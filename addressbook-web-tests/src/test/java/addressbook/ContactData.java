40dd247c563921fd8c0972b19a014535c28c09a8
DmitryTeterkin <teterkin@tut.by>
2017-01-25 23:08:08 +0300

    Задание 4. Тест. Создание нового контакта, выделение вспомогательного класса

---
+++ addressbook-web-tests/src/test/java/addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package addressbook;
+
+public class ContactData {
+  private final String firstName;
+  private final String secondName;
+  private final String nickName;
+  private final String address;
+  private final String email;
+
+  public ContactData(String firstName, String secondName, String nickName, String address, String email) {
+    this.firstName = firstName;
+    this.secondName = secondName;
+    this.nickName = nickName;
+    this.address = address;
+    this.email = email;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getSecondName() {
+    return secondName;
+  }
+
+  public String getNickName() {
+    return nickName;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
