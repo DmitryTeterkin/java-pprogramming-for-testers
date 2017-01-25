e3fd5c7cb56c44ed49b96312ee28ccb0350d6da9
DmitryTeterkin <teterkin@tut.by>
2017-01-25 20:29:58 +0300

    Тест вычисления длины отрезка. Задание 3

--- sandbox/src/test/java/workbook/SecondExercise/segmentLengthTests.java
+++ sandbox/src/test/java/workbook/SecondExercise/segmentLengthTests.java
@@ -14,9 +14,12 @@ public class segmentLengthTests {
 
     Point p1 = new Point(12, 13);
     Point p2 = new Point(-12, -13);
+     // сравнение через метод
+    Assert.assertEquals(p1.distanceMethod(p1, p2), 35.38361202590826);
 
     Point p3 = new Point(-12, 13);
     Point p4 = new Point(-2, -13);
+     // сравнение через функцию
     Assert.assertEquals(Point.distanceFuncion(p3, p4), 27.85677655436824);
 
 
