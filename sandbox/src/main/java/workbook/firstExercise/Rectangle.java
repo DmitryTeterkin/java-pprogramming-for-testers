f364f5ef3f6b944a9a841ad9829105b3c6ca53f2
DmitryTeterkin <teterkin@tut.by>
2017-01-25 19:47:35 +0300

    добавил задание
    вычисление площади квадрата и прямоугольника

---
+++ sandbox/src/main/java/workbook/firstExercise/Rectangle.java
@@ -0,0 +1,19 @@
+package workbook.firstExercise;
+
+/**
+ * Created by Dmitry on 25.01.2017.
+ */
+public class Rectangle {
+  public double a;
+  public double b;
+
+  public Rectangle (double a, double b) {
+    this.a = a;
+    this.b = b;
+  }
+  // функция вычисления площади прямоугольника
+  public double area () {
+    return this.a * this.b;
+  }
+
+}
---
+++ sandbox/src/main/java/workbook/firstExercise/Square.java
@@ -0,0 +1,17 @@
+package workbook.firstExercise;
+
+/**
+ * Created by Dmitry on 25.01.2017.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public double area() {
+    return this.l * this.l;
+  }
+}
