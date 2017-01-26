ca28f59ef48094266b013ac9f955d5b0e1825fa9
DmitryTeterkin <teterkin@tut.by>
2017-01-26 16:10:14 +0300

    Задание 5. Создание HelperBase для вспомогательных классов.
    Перенес в него низкоуровневые вспомогательные методы: type и click.

--- addressbook-web-tests/src/test/java/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/addressbook/appmanager/ContactHelper.java
@@ -7,35 +7,25 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by dteterkin on 26.01.2017.
  */
-public class ContactHelper {
- private FirefoxDriver wd;
+public class ContactHelper extends HelperBase {
+
 
   public ContactHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super (wd);
   }
 
   // подтверждение создания контакта
   public void submitContactCreation() {
-    wd.findElement(By.name("submit")).click();
+    click(By.name("submit"));
   }
 
   // заполнение контакта
   public void fillContactForm(ContactData contactData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(contactData.getSecondName());
-    wd.findElement(By.name("middlename")).click();
-    wd.findElement(By.name("nickname")).click();
-    wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys(contactData.getNickName());
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    type(By.name("firstname"), contactData.getFirstName());
+    type(By.name("lastname"), contactData.getSecondName());
+    type(By.name("nickname"), contactData.getNickName());
+    type(By.name("address"), contactData.getAddress());
+    type(By.name("email"), contactData.getEmail());
+
   }
 }
--- addressbook-web-tests/src/test/java/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/addressbook/appmanager/NavigationHelper.java
@@ -6,25 +6,25 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by dteterkin on 26.01.2017.
  */
-public class NavigationHelper {
- private FirefoxDriver wd;
+public class NavigationHelper extends HelperBase {
+
 
   public NavigationHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   // возврат на страницу группы
   public void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
+    click(By.linkText("groups"));
   }
 
   // возврат на страницу контактов
   public void returnToContactList() {
-    wd.findElement(By.linkText("home page")).click();
+    click(By.linkText("home page"));
   }
 
-  // добавление контакта
+  // переход на страницу создания контакта
   public void gotoContactEditorPage() {
-    wd.findElement(By.linkText("add new")).click();
+    click(By.linkText("add new"));
   }
 }
