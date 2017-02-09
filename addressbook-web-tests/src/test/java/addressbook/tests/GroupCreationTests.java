f56a198aabfaa6c1a587f7ad0c369efe515ff706
DmitryTeterkin <teterkin@tut.by>
2017-02-09 17:44:50 +0300

    лекция 4.9 вычисление максимального номера идентификатора с помощью лямбда функций

--- addressbook-web-tests/src/test/java/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/addressbook/tests/GroupCreationTests.java
@@ -3,7 +3,6 @@ package addressbook.tests;
 import addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,19 +12,13 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage(); // преход на страницу групп
     List<GroupData> before = app.getGroupHelper().getGroupList(); // построение списка групп до добавления новой группы
-    GroupData group = new GroupData("test1", "test1", "test2"); // перенос атрибутов новой группы в переменную
+    GroupData group = new GroupData("test7", "test1", "test2"); // перенос атрибутов новой группы в переменную
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList(); // построение списка групп после добавления новой группы
     Assert.assertEquals(after.size(), before.size() + 1); // сравнение количества групп до и после добавления новой группы
-// поиск группы с максимальным идентификатором
-    int max = 0;
-    for (GroupData g : after){
-      if (g.getId() > max){
-        max = g.getId();
-      }
-    }
 
-    group.setId(max);
+// поиск группы с максимальным идентификатором с помощью потоков
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group); // добавление в коллекцию "до" новой созданной группы
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after)); // сравнение двух коллекций групп
 
