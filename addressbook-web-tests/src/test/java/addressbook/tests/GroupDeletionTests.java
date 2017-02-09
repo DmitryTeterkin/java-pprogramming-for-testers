1ed3526416b6d2ca87cdca6a1aad150e9d7993fb
DmitryTeterkin <teterkin@tut.by>
2017-02-09 09:48:06 +0300

    лекция 4.3 сделаны проверки на количество групп до и после изменения или удаления группы

--- addressbook-web-tests/src/test/java/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/addressbook/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package addressbook.tests;
 
 import addressbook.model.GroupData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
@@ -14,9 +15,12 @@ public class GroupDeletionTests extends TestBase {
     if (! app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupData("test1", "test1", "test2"));
     }
+    int before = app.getGroupHelper().getGroupCount(); // количество групп до добавления
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
+    int after = app.getGroupHelper().getGroupCount(); // количество групп после добавления новой группы
+    Assert.assertEquals(after, before - 1); // сравнение количества групп до и после добавления новой группы
   }
 
 }
--- addressbook-web-tests/src/test/java/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/addressbook/tests/GroupModificationTests.java
@@ -1,6 +1,7 @@
 package addressbook.tests;
 
 import addressbook.model.GroupData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
@@ -14,12 +15,14 @@ public class GroupModificationTests extends TestBase {
     if (! app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupData("test1", "test1", "test2"));
     }
-    //
+    int before = app.getGroupHelper().getGroupCount(); // количество групп до добавления
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
 // оставляем дефолтные поля при изменении группы
     app.getGroupHelper().fillGroupForm(new GroupData("test5", null, null));
-    app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
+    int after = app.getGroupHelper().getGroupCount(); // количество групп после добавления новой группы
+    Assert.assertEquals(after, before); // сравнение количества групп до и после добавления новой группы
   }
 }
