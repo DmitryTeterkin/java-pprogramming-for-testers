2ebc84d26fe2a4b94b1b8c2521feebf033abf0ce
DmitryTeterkin <teterkin@tut.by>
2017-02-11 12:50:19 +0300

    задание 4.8 проблема с определением id как интежер

--- addressbook-web-tests/src/test/java/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/addressbook/tests/ContactCreationTests.java
@@ -5,6 +5,7 @@ import addressbook.model.ContactData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
@@ -14,10 +15,22 @@ public class ContactCreationTests extends TestBase {
   public void testContactCreation() {
     List<ContactData> before = app.getContactHelper().getContactList();
     app.getNavigationHelper().gotoContactEditorPage(); // переход на страницу редактирования контакта
-    app.getContactHelper().createContact(new ContactData(null,"petro", "petrov", "NiKnAmE", "testovii address", "test@test.com", "[none]"), true);
+    ContactData contact = new ContactData("petro", "petrov", "NiKnAmE", "testovii address", "test@test.com", "[none]");
+    app.getContactHelper().createContact(contact, true);
     app.getNavigationHelper().gotoHomePage();         // возврат на список контактов
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1); // сравниваем количество контактов до и после
+
+   // в списке after находим контакт с максимальным идентификатором (предполагая что он будет у только что созданного контакта)
+   int max = 0;
+   for (ContactData c : after){
+     if (c.getId() > max) {
+       max = c.getId();
+     }
+   }
+   contact.setId(max);// присваиваем контакту максимальный найденный идентификатор
+   before.add(contact); // добавляем в список before новый контакт
+   Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after)); // сравнивание списков, преобразованных в неотсортированные множества
   }
 
 }
