eda343a2642794aa95076a2776fd7e726b08f938
DmitryTeterkin <teterkin@tut.by>
2017-02-11 11:27:10 +0300

    лекция 4.6 для контактов. сравнение списокв целиком при удалении контакта.

--- addressbook-web-tests/src/test/java/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/addressbook/tests/ContactDeletionTests.java
@@ -1,14 +1,12 @@
 package addressbook.tests;
 
 import addressbook.model.ContactData;
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+
 
 
 public class ContactDeletionTests extends TestBase {
@@ -26,8 +24,13 @@ public class ContactDeletionTests extends TestBase {
     List<ContactData> before = app.getContactHelper().getContactList(); // создаем список контактов до удаления
     app.getContactHelper().selectContact(before.size() - 1);
     app.getContactHelper().deletionContact();
-    app.getNavigationHelper().returnToContactList();
+    app.getNavigationHelper().gotoHomePage();
     List<ContactData> after = app.getContactHelper().getContactList(); // создаем список контактов после удаления
     Assert.assertEquals(after.size(), before.size() - 1); // сравниваем количество контактов до и после удаления
+
+
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after); // сравнение двух списков как списков, а не по количеству контактов.
+
   }
 }
