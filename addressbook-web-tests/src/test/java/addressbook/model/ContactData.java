f7cf491d546cce65732fba2ea5debd1b4f697901
DmitryTeterkin <teterkin@tut.by>
2017-02-13 09:38:20 +0300

    Revert "задание 4.8 проблема с определением id как интежер"
    This reverts commit 2ebc84d26fe2a4b94b1b8c2521feebf033abf0ce.

--- addressbook-web-tests/src/test/java/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/addressbook/model/ContactData.java
@@ -1,7 +1,7 @@
 package addressbook.model;
 
 public class ContactData {
-  private int id;
+  private final String id;
   private final String firstName;
   private final String secondName;
   private final String nickName;
@@ -10,7 +10,7 @@ public class ContactData {
   private final String email;
   // конструктор без ID
   public ContactData(String firstName, String secondName, String nickName, String address, String email, String group) {
-    this.id = 0;
+    this.id = null;
     this.firstName = firstName;
     this.secondName = secondName;
     this.nickName = nickName;
@@ -19,7 +19,7 @@ public class ContactData {
     this.group = group;
   }
 
-  public ContactData(int id, String firstName, String secondName, String nickName, String address, String email, String group) {
+  public ContactData(String id, String firstName, String secondName, String nickName, String address, String email, String group) {
     this.id = id;
     this.firstName = firstName;
     this.secondName = secondName;
@@ -29,11 +29,7 @@ public class ContactData {
     this.group = group;
   }
 
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  public int getId() {
+  public String getId() {
     return id;
   }
 
@@ -77,14 +73,14 @@ public class ContactData {
 
     ContactData that = (ContactData) o;
 
-    if (id != that.id) return false;
+    if (id != null ? !id.equals(that.id) : that.id != null) return false;
     if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
     return secondName != null ? secondName.equals(that.secondName) : that.secondName == null;
   }
 
   @Override
   public int hashCode() {
-    int result = id;
+    int result = id != null ? id.hashCode() : 0;
     result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
     result = 31 * result + (secondName != null ? secondName.hashCode() : 0);
     return result;
