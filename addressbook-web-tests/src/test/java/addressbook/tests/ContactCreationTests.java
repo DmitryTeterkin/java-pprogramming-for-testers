7f07d6d7235a052235cc4e9a4bb1ff269b766d33
DmitryTeterkin <teterkin@tut.by>
2017-02-22 10:06:39 +0300

    Создание контактов из xml файла - работает

--- addressbook-web-tests/src/test/java/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/addressbook/tests/ContactCreationTests.java
@@ -3,13 +3,14 @@ package addressbook.tests;
 
 import addressbook.model.ContactData;
 import addressbook.model.Contacts;
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -19,6 +20,22 @@ public class ContactCreationTests extends TestBase {
 
   @DataProvider
   public Iterator<Object[]> validContacts() throws IOException { // итератор массивов объектов
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null){
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+    return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
+  }
+
+ /*       чтение данных из файла csv формата
+
+ public Iterator<Object[]> validContacts() throws IOException { // итератор массивов объектов
     List<Object[]> list = new ArrayList<>();
     BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
     String line = reader.readLine();
@@ -27,9 +44,9 @@ public class ContactCreationTests extends TestBase {
       list.add(new Object[] {new ContactData().withFirstName(split[0]).withSecondName(split[1]).withGroup(split[2]).withMobilePhone(split[3])});
       line = reader.readLine();
     }
-
     return list.iterator();
   }
+*/
 
   @Test (dataProvider = "validContacts") //(enabled = false)
   public void testContactCreation(ContactData contact) {
@@ -38,11 +55,8 @@ public class ContactCreationTests extends TestBase {
     app.contact().create(contact, true);
     app.goTo().homePage();         // возврат на список контактов
 
-
     assertThat(app.contact().count(), equalTo(before.size() + 1)); // сравниваем размеры списков до и после изменения
     Contacts after = app.contact().all();
-
-
     assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));// сравнивание списков, преобразованных в неотсортированные множества
   }
 
