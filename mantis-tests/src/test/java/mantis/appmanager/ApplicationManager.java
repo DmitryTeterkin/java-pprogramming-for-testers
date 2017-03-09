e3b9c88859fb0fc89ee8fec3b5b2186f4a2272ed
DmitryTeterkin <teterkin@tut.by>
2017-03-09 17:00:18 +0300

    лекция 8.5 замена инишника с помощью ftp для теста регистрации (отключение капчи)

--- mantis-tests/src/test/java/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/mantis/appmanager/ApplicationManager.java
@@ -17,9 +17,12 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationManager {
 
   private final Properties properties;
-  WebDriver wd;
+  private WebDriver wd;
 
   private String browser;
+  private RegistrationHelper registrationHelper;
+  private FtpHelper ftp;
+
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -30,19 +33,12 @@ public class ApplicationManager {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
-    if (Objects.equals(browser, BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-    } else if (Objects.equals(browser, BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (Objects.equals(browser, BrowserType.IE)) {
-      wd = new InternetExplorerDriver();
-    }
-    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
   }
 
   public void stop() {
-    wd.quit();
+    if (wd != null) {
+      wd.quit();
+    }
   }
 
   public HttpSession newSession(){
@@ -54,6 +50,32 @@ public class ApplicationManager {
   }
 
   public RegistrationHelper registration() {
-    return new RegistrationHelper(this);
+    if (registrationHelper == null) {
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+  }
+
+  public FtpHelper ftp(){
+   if (ftp == null) {
+     ftp = new FtpHelper(this);
+   }
+   return ftp;
+  }
+
+
+  public WebDriver getDriver() {
+    if (wd == null){
+      if (Objects.equals(browser, BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (Objects.equals(browser, BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      } else if (Objects.equals(browser, BrowserType.IE)) {
+        wd = new InternetExplorerDriver();
+      }
+      wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+      wd.get(properties.getProperty("web.baseUrl"));
+    }
+    return wd;
   }
 }
--- mantis-tests/src/test/java/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/mantis/appmanager/RegistrationHelper.java
@@ -2,18 +2,18 @@ package mantis.appmanager;
 
 import org.openqa.selenium.WebDriver;
 
-/**
- * Created by Dmitry on 08.03.2017.
- */
+
 public class RegistrationHelper {
   private final ApplicationManager app;
   private WebDriver wd;
 
   public RegistrationHelper(ApplicationManager app) {
     this.app = app;
+    wd = app.getDriver();
   }
 
   public void start(String username, String email) {
+    wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
 
   }
 }
