bf5b480fc1e78ec82ad3e3608af2d90589cc94ec
DmitryTeterkin <teterkin@tut.by>
2017-03-15 23:27:32 +0300

    Начал делать 21 задание.

--- mantis-tests/src/test/java/mantis/tests/SoapTests.java
+++ mantis-tests/src/test/java/mantis/tests/SoapTests.java
@@ -1,7 +1,6 @@
 package mantis.tests;
 
 
-
 import mantis.model.Issue;
 import mantis.model.Project;
 import org.testng.annotations.Test;
@@ -16,21 +15,25 @@ import static org.testng.Assert.assertEquals;
 public class SoapTests extends TestBase {
 
   @Test
-  public void testGetProgects () throws MalformedURLException, ServiceException, RemoteException {
+  public void testGetProgects() throws MalformedURLException, ServiceException, RemoteException {
+    int issueId = 1;
+    skipIfNotFixed(issueId);
     Set<Project> projects = app.soap().getProjects();
     System.out.println(projects.size());
-    for (Project project : projects){
+    for (Project project : projects) {
       System.out.println(project.getName());
     }
   }
 
   @Test
   public void testCreateIssue() throws RemoteException, ServiceException, MalformedURLException {
+    int issueId = 0;
+    skipIfNotFixed(issueId);
     Set<Project> projects = app.soap().getProjects();
     Issue issue = new Issue().withSummary("Test issue")
             .withDescription("Test issue description").withProject(projects.iterator().next());
-  Issue created = app.soap().addIssue(issue);
-   assertEquals(issue.getSummary(), created.getSummary());
+    Issue created = app.soap().addIssue(issue);
+    assertEquals(issue.getSummary(), created.getSummary());
   }
 
 
