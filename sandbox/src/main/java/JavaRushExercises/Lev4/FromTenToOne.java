3ae002e0b6681295802b6342acfe330d48ee4c32
DmitryTeterkin <teterkin@tut.by>
2018-03-07 10:02:04 +0300

    От 10 до 1
    Вывести на экран числа от 10 до 1 используя цикл while. Каждое значение
    с новой строки.

--- sandbox/src/main/java/JavaRushExercises/Lev4/FromTenToOne.java
+++ sandbox/src/main/java/JavaRushExercises/Lev4/FromTenToOne.java
@@ -12,4 +12,11 @@ package JavaRushExercises.Lev4;
 5. В программе должен использоваться цикл while.
  */
 public class FromTenToOne {
+  public static void main(String[] args) {
+    int i = 10;
+    while (i != 0){
+      System.out.println(i);
+      i--;
+    }
+  }
 }
