cd830f1ed1a633c2f3161ad21ba33d42c82b92f4
DmitryTeterkin <teterkin@tut.by>
2018-03-07 14:11:35 +0300

    Описываем числа

--- sandbox/src/main/java/JavaRushExercises/Lev4/NumbersSolution.java
+++ sandbox/src/main/java/JavaRushExercises/Lev4/NumbersSolution.java
@@ -30,5 +30,28 @@ package JavaRushExercises.Lev4;
 10. Если введенное число не попадает в диапазон 1 - 999, ничего не выводить на экран
  */
 
+import java.util.Scanner;
+
 public class NumbersSolution {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    if (a >= 0 && a < 1000) {
+      if (a > 99 && a % 2 == 0) {
+        System.out.println("четное трехзначное число");
+      } else if ((a > 99 && a % 2 != 0)) {
+        System.out.println("нечетное трехзначное число");
+      }
+      if (a > 9 && a < 99 && a % 2 == 0) {
+        System.out.println("четное двузначное число");
+      } else if (a > 9 && a < 99 && a % 2 != 0) {
+        System.out.println("нечетное двузначное число");
+      }
+      if (a > 0 && a < 10 && a % 2 == 0) {
+        System.out.println("четное однозначное число");
+      } else if (a > 0 && a < 10 && a % 2 != 0) {
+        System.out.println("нечетное однозначное число");
+      }
+    }
+  }
 }
