225832d64ced84894997457ee8824fe6dcce26a0
DmitryTeterkin <teterkin@tut.by>
2018-03-07 09:51:39 +0300

    Положительное число
    Ввести с клавиатуры три целых числа. Вывести на экран количество
    положительных чисел в исходном наборе.

--- sandbox/src/main/java/JavaRushExercises/Lev4/CountPlusNumbers.java
+++ sandbox/src/main/java/JavaRushExercises/Lev4/CountPlusNumbers.java
@@ -1,4 +1,7 @@
 package JavaRushExercises.Lev4;
+
+import java.util.Scanner;
+
 /*
 Положительное число
 Ввести с клавиатуры три целых числа. Вывести на экран количество положительных чисел в исходном наборе.
@@ -25,5 +28,18 @@ package JavaRushExercises.Lev4;
 4. Если положительных чисел нет, программа должна вывести "0".
  */
 public class CountPlusNumbers {
-
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int[] mas = new int[3];
+    mas[0] = scanner.nextInt();
+    mas[1] = scanner.nextInt();
+    mas[2] = scanner.nextInt();
+    int pol = 0;
+    for (int i = 0; i < 3; i++) {
+      if (mas[i] > 0) {
+        pol = pol + 1;
+      }
+    }
+    System.out.println(pol);
+  }
 }
