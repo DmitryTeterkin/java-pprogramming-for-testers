31591726270d7f21f5437ffe61a37c57e53d0dd9
DmitryTeterkin <teterkin@tut.by>
2018-08-29 20:51:47 +0300

    new tasks
    new tasks

---
+++ sandbox/src/main/java/Lev14/Task1417/Money.java
@@ -0,0 +1,10 @@
+package Lev14.Task1417;
+
+public abstract class Money {
+  public Money(double amount) {
+  }
+
+  public abstract double getAmount();
+
+  public abstract String getCurrencyName();
+}
