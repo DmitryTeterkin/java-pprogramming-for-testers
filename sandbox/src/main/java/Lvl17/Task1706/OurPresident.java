72f6eece40c4096173fa4eea20b83394e79ec15e
DmitryTeterkin <teterkin@tut.by>
2019-02-13 08:17:13 +0300

    new tasks

---
+++ sandbox/src/main/java/Lvl17/Task1706/OurPresident.java
@@ -0,0 +1,13 @@
+package Lvl17.Task1706;
+
+public class OurPresident {
+  private static OurPresident president;
+
+  private OurPresident() {
+  }
+
+  public static OurPresident getOurPresident() {
+    return president;
+  }
+}
+
---
+++ sandbox/src/main/java/Lvl17/Task1707/IMF.java
@@ -0,0 +1,14 @@
+package Lvl17.Task1707;
+
+public class IMF {
+
+  private static IMF imf;
+
+  public static IMF getFund() {
+    //add your code here - добавь код тут
+    return imf;
+  }
+
+  private IMF() {
+  }
+}
